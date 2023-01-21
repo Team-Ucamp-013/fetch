9e1d14cf09c02cb84a28a5fee90985ba617c9a9f
Alfredo Hernandez Noriega <alfredo.hernandez@kushkipagos.com>
2023-01-21 10:43:10 -0600

    fetch clima

--- index.js
+++ index.js
@@ -7,12 +7,28 @@ console.log('hola')
 //     .then(data => console.log(data))
 // }
 
-const getDataCurrency = async () => {
-  let url = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur/mxn.json`
+// const getDataCurrency = async () => {
+//   let url = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur/mxn.json`
+//   const response = await fetch(url)
+//   console.log('response', response)
+//   const data = await response.json()
+//   console.log('data', data)
+// }
+
+// getDataCurrency()
+
+
+const getWeather = async () => {
+  let ciudad = document.getElementById('ciudad').value
+  console.log(ciudad)
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=616629f9acdc3b22b8b09553e632e5da`
   const response = await fetch(url)
   console.log('response', response)
   const data = await response.json()
-  console.log('data', data)
+  const { main } = data
+  console.log(`La temperatura en ${ciudad} es de ${main.temp}`)
+
 }
 
-getDataCurrency()
+
+
